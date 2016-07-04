d3ecd55d25390ee3e18481058454fa25d4b7299e
Loxstomper <Loxstomper@users.noreply.github.com>
2016-07-04 13:31:02 +1000

    updated client

--- client.py
+++ client.py
@@ -1,19 +1,37 @@
 import socket
 
+# HOST = socket.gethostbyname('raspberrypi')
 HOST = input("Host: ")
 PORT = 5000
 
 
-def move(direction):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def send_to_server(info):
     try:
-        s.connect((HOST, PORT))
-        s.send(direction.encode())
-        s.close()
-    except:
-        print("Failed to establish a connection with the server.")
+        s.send(info.encode())
+    except Exception as e:
+        print(e)
+
+
+def controls():
+    print("8 | Forward")
+    print("2 | Reverse")
+    print("0 | Stop")
+    print("4 | Turn Left")
+    print("6 | Turn Right")
+    print("5 | Centre Steering")
+
+print("Attempting to connect to the server")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect((HOST, PORT))
+    print("Successfully connected to the server")
+    controls()
+except Exception as e:
+    print(e)
 
 while True:
     data = input("Enter a direction: ")
-    move(data)
+    send_to_server(data)
+    reply = s.recv(1024)
+    print(reply.decode())
 
